089eed44c075a8ef06cf35f841c7f1ac4df049e9
william2548 <34666923+william2548@users.noreply.github.com>
2018-03-09 21:15:25 -0700

    Initialization of ASTParser
    Did some playing around with the parser

--- Project.java
+++ Project.java
@@ -1,10 +1,56 @@
 package seng_project;
 
-public class Project {
 
-	public static void main(String[] args) {
-		System.out.println("Test");
-		int time;
-	}
+
+import java.util.Map;
+import java.util.Scanner;
+
+import org.eclipse.jdt.core.JavaCore;
+import org.eclipse.jdt.core.dom.AST;
+import org.eclipse.jdt.core.dom.ASTParser;
+import org.eclipse.jdt.core.dom.ASTVisitor;
+import org.eclipse.jdt.core.dom.ArrayCreation;
+import org.eclipse.jdt.core.dom.ArrayType;
+import org.eclipse.jdt.core.dom.Block;
+import org.eclipse.jdt.core.dom.CompilationUnit;
+import org.eclipse.jdt.core.dom.FieldDeclaration;
+import org.eclipse.jdt.core.dom.TypeDeclaration;
+public class JavaApplication {
+		public static void main(String[] args) {
+			int decCount = 0;
+			int refCount = 0;
+			Project p = new Project();
+			Scanner input = new Scanner(System.in);
+			//System.out.print("Enter a file: ");
+			//String fileName = input.nextLine();
+			p.initParser("public class A {ArrayList<String> string = new ArrayList<String>; int seconds;} private class B{} char C;}");
+		}
+		
+		public void initParser(String fileName) {
+
+			ASTParser parser = ASTParser.newParser(AST.JLS8);
+			parser.setSource(fileName.toCharArray());						
+			parser.setKind(parser.K_COMPILATION_UNIT);
+			Map options = JavaCore.getOptions();
+			JavaCore.setComplianceOptions(JavaCore.VERSION_1_8, options);
+			parser.setCompilerOptions(options);
+			CompilationUnit unit = (CompilationUnit)parser.createAST(null);
+			unit.accept(new ASTVisitor() {
+				
+				public boolean visit(TypeDeclaration node) {
+					System.out.println("Type Declaration: " + node.getName());
+
+					return true;
+				}
+				public boolean visit(FieldDeclaration node) {
+					System.out.println("Field Declaration: " + node.getType());
+					return true;
+				}
+
+				
+			});
+		}
+
+		
 
 }
