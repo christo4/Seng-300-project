4bdc4dd84c6a845f0d6abbb8f5aec042fb7de8e6
christo4 <christopher.rodrigue@ucalgary.ca>
2018-03-11 22:44:36 -0600

    Add files via upload

--- Project.java
+++ Project.java
@@ -1,5 +1,10 @@
+package seng_project;
 
-
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FilenameFilter;
+import java.io.IOException;
 import java.util.Map;
 import java.util.Scanner;
 
@@ -16,27 +21,77 @@ import org.eclipse.jdt.core.dom.EnumDeclaration;
 import org.eclipse.jdt.core.dom.FieldDeclaration;
 import org.eclipse.jdt.core.dom.SimpleName;
 import org.eclipse.jdt.core.dom.TypeDeclaration;
+
 public class Project {
 		public static int refCount = 0;
 		public static int decCount = 0;
 		public static void main(String[] args) {
 
 			Project p = new Project();
+			String pathName = "";
+			String javaType = "";
 			
-			//System.out.print("Enter Java Type: ");
-			//String type = input.nextLine();
-			p.initParser("public class A {String name; char c; Time D;} public class B{}");
-
+			if(args.length == 2) {
+				pathName = args[0];
+				javaType = args[1];
+			}
+			else {
+				System.out.println("Unallowed amount of arguments. Ending program.");
+				System.exit(1);
+			}
+			p.initParser(p.getDirectory(pathName), javaType);
 		}
 		
-		public void initParser(String javaFiles) {
-			Scanner input = new Scanner(System.in);
-			System.out.print("Enter Type: ");
-			String javaType = input.nextLine();
+		public String getDirectory(String pathName) {
+			File temp;
+			File file = new File(pathName);
+			File[] list = file.listFiles(new FilenameFilter() {
+				public boolean accept(File file, String name)
+				{
+					return name.endsWith(".java");
+				}
+			});
+			StringBuffer sourceCode = new StringBuffer();
+			for(int i = 0; i<list.length; i++) {
+				temp = list[i];
+				sourceCode.append(fileToString(temp,sourceCode));
+			}
+			return sourceCode.toString();
+		}
+		
+		public StringBuffer fileToString(File fileToBeRead, StringBuffer sourceCode) {
+			
+			String line;
+			String code = "";
+			BufferedReader br = null;
+			try {
+				br = new BufferedReader(new FileReader(fileToBeRead));
+				while ((line = br.readLine()) != null) {
+					code = code + line;
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			} finally {
+				try {
+					if (br != null) {
+						br.close();
+					}
+				} catch (IOException ex) {
+					ex.printStackTrace();
+				}
+			}
+			return sourceCode.append(code);
+		}
+		
+		public void initParser(String sourceCode, String javaType) {
+			//Scanner input = new Scanner(System.in);
+			//System.out.print("Enter Type: ");
+			//String javaType = input.nextLine();
 			
 			ASTParser parser = ASTParser.newParser(AST.JLS8);
-			parser.setSource(javaFiles.toCharArray());						
+			parser.setSource(sourceCode.toCharArray());						
 			parser.setKind(parser.K_COMPILATION_UNIT);
+			parser.setResolveBindings(true);
 			Map options = JavaCore.getOptions();
 			JavaCore.setComplianceOptions(JavaCore.VERSION_1_8, options);
 			parser.setCompilerOptions(options);
@@ -87,6 +142,3 @@ public class Project {
 		}
 		
 }
-
-		
-
